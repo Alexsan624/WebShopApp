367359ce5943c4a304f0671417c9e12ea19018fd
Alexsan624 <sashkosan624@gmail.com>
2023-12-18 11:36:56 +0200

    Part 5 - Not ready yet

---
+++ WebShopApp/WebShopApp/Models/Order/OrderCreateVM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
+
+using Humanizer.Localisation.TimeToClockNotation;
+
+namespace WebShopApp.Models.Order
+{
+    public class OrderCreateVM
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int QuantityInStock { get; set; }
+        public string? Picture { get; set; }
+
+        [Range(1, 100)]
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
