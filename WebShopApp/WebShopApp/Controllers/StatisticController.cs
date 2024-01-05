a5e9e6ee90449b672cc0c60a10e8a3d24b619525
Alexsan624 <sashkosan624@gmail.com>
2024-01-05 11:19:32 +0200

    Part 6 and Part 7

---
+++ WebShopApp/WebShopApp/Controllers/StatisticController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+using WebShopApp.Core.Contracts;
+using WebShopApp.Models.Statistic;
+
+namespace WebShopApp.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private readonly IStatisticService _statisticService;
+
+        public StatisticController(IStatisticService statisticService)
+        {
+            this._statisticService = statisticService;
+        }
+        public IActionResult Index()
+        {
+            StatisticVM statistics = new StatisticVM();
+            statistics.CountClients = _statisticService.CountClients();
+            statistics.CountProducts = _statisticService.CountProdcts();
+            statistics.CountOrders = _statisticService.CountOrders();
+            statistics.SumOrders = _statisticService.SumOrders();
+            return View(statistics);
+        }
+    }
+}
